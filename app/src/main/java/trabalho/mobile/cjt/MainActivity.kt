f270299ee2956afebd8d9246ab70dd8763f8c70f
jvpts11 <jvptsyt11@gmail.com>
2024-01-03 11:00:09 +0000

    made bottom navigation menu

--- app/src/main/java/trabalho/mobile/cjt/MainActivity.kt
+++ app/src/main/java/trabalho/mobile/cjt/MainActivity.kt
@@ -2,10 +2,34 @@ package trabalho.mobile.cjt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import trabalho.mobile.cjt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /*
+        binding.mainBottomNav.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+            }
+
+        }
+        */
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.main_frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
