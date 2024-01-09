f768a06763c7b94d7361f19a3034dacbe4b6dff8
jvpts11 <jvptsyt11@gmail.com>
2024-01-09 01:24:08 +0000

    e necessário ajeitar um bug relacionado a imagem crashar o programa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,6 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -42,7 +41,9 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.github.dhaval2404:imagepicker:2.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -51,6 +52,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation("com.google.firebase:firebase-firestore:24.10.0")
     implementation("com.google.firebase:firebase-database-ktx:20.3.0")
+    implementation("com.google.firebase:firebase-storage:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/trabalho/mobile/cjt/fragments/NewsFragment.kt
+++ app/src/main/java/trabalho/mobile/cjt/fragments/NewsFragment.kt
@@ -1,26 +1,31 @@
 package trabalho.mobile.cjt.fragments
 
-import android.content.Context
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import com.google.protobuf.Value
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import trabalho.mobile.cjt.ProfileActivity
-import trabalho.mobile.cjt.R
 import trabalho.mobile.cjt.User
 import trabalho.mobile.cjt.UserGamesActivity
-import trabalho.mobile.cjt.databinding.FragmentMemberListBinding
 import trabalho.mobile.cjt.databinding.FragmentNewsBinding
 
 class NewsFragment : Fragment() {
@@ -35,6 +40,32 @@ class NewsFragment : Fragment() {
     private lateinit var profileAccessButton : Button
     private lateinit var userGamesAccessButton : Button
 
+    private lateinit var newsImage : ImageView
+
+    private lateinit var storageRef: StorageReference
+    private var selectedImageUri: Uri? = null
+
+    private val imagePickerLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            selectedImageUri = data?.data
+
+            // Verifica se a URI da imagem selecionada é válida
+            if (selectedImageUri != null) {
+                // A imagem já foi selecionada, pode fazer o upload para o Firebase aqui se necessário
+                uploadImageToFirebase(selectedImageUri!!)
+            } else {
+                // A imagem ainda não foi selecionada, permita que o usuário escolha uma
+                selectImage()
+            }
+
+            Glide.with(this)
+                .load(selectedImageUri)
+                .apply(RequestOptions.circleCropTransform())
+                .into(newsImage)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -45,6 +76,8 @@ class NewsFragment : Fragment() {
         if (uid.isNotEmpty()){
             getUserData()
         }
+
+        storageRef = FirebaseStorage.getInstance().reference.child("News")
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -52,6 +85,7 @@ class NewsFragment : Fragment() {
 
         profileAccessButton = binding.profileAccessButton
         userGamesAccessButton = binding.userGamesAccessButton
+        newsImage = binding.newsImage
 
         profileAccessButton.setOnClickListener(){
             val intent = Intent(activity, ProfileActivity::class.java)
@@ -63,8 +97,60 @@ class NewsFragment : Fragment() {
             startActivity(intent)
         }
 
+        newsImage.setOnClickListener {
+            // Verifica se a URI da imagem selecionada é válida
+            if (selectedImageUri != Uri.EMPTY) {
+                // A imagem já foi selecionada, pode fazer o upload para o Firebase aqui se necessário
+                uploadImageToFirebase(selectedImageUri!!)
+            } else {
+                // A imagem ainda não foi selecionada, permita que o usuário escolha uma
+                selectImage()
+            }
+        }
+
         return binding.root
     }
+
+    private fun selectImage() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        imagePickerLauncher.launch(intent)
+    }
+
+    private fun uploadImageToFirebase(uri: Uri) {
+        val imageName = getFileName(uri) // Obter o nome da imagem a partir da URI
+        val imageRef = storageRef.child(imageName)
+
+        // Upload da imagem para o Firebase Storage
+        imageRef.putFile(uri)
+            .addOnSuccessListener { taskSnapshot ->
+                // Imagem foi carregada com sucesso
+                Toast.makeText(requireContext(), "Imagem carregada com sucesso!", Toast.LENGTH_SHORT).show()
+
+                // Você pode obter a URL da imagem carregada para salvar no banco de dados, se necessário
+                imageRef.downloadUrl.addOnSuccessListener { downloadUrl ->
+                    val imageUrl = downloadUrl.toString()
+                    // Aqui você pode salvar a URL da imagem no banco de dados se necessário
+                    // Exemplo: saveImageUrlToDatabase(imageUrl)
+                }
+            }
+            .addOnFailureListener { e ->
+                // O upload da imagem falhou
+                Toast.makeText(requireContext(), "Falha ao carregar imagem: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun getFileName(uri: Uri): String {
+        // Obter o nome do arquivo a partir da URI
+        val cursor = requireContext().contentResolver.query(uri, null, null, null, null)
+        cursor?.use {
+            it.moveToFirst()
+            val displayName = it.getString(it.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME))
+            return displayName ?: "user_image.jpg" // Nome padrão caso não seja possível obter o nome
+        }
+        return "user_image.jpg" // Nome padrão caso ocorra algum problema
+    }
+
 
     private fun getUserData(){
         dbRef.child(uid).addValueEventListener(object : ValueEventListener{
